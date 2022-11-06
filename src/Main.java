f99fc7f024f04e289194acb7bba8650daab82c86
unknown <trongtoanbk.hd@gmail.com>
2022-11-06 11:37:04 +0700

    cau dieu kien

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -46,14 +48,43 @@ public class Main {
 //        System.out.println(a);
 //        System.out.println(c);
 
-        int a= 5;
-        int b=5;
-        int result = a++ - --b + b-- +a-- - a++ + b++ - b--;
-        System.out.println(result);
+//        int a= 5;
+//        int b=5;
+//        int result = a++ - --b + b-- +a-- - a++ + b++ - b--;
+//        System.out.println(result);
 
+        //4. Câu điều kiện if
+        // Ví dụ: Kiểm tra xem có phải năm nhuận không, nếu là năm nhuận thì hiện ra thông báo
+        int year;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Nhập vào năm bạn cần kiểm tra:");
+        year = scan.nextInt();
+        scan.close();
+        boolean isLeap = false;
+        if(year % 4 == 0)//chia hết cho 4 là năm nhuận
+        {
+            if( year % 100 == 0)
+            //nếu vừa chia hết cho 4 mà vừa chia hết cho 100 thì không phải năm nhuận
+            {
+                if ( year % 400 == 0)//chia hết cho 400 là năm nhuận
+                    isLeap = true;
+                else
+                    isLeap = false;//không chia hết cho 400 thì không phải năm nhuận
+            }
+            else//chia hết cho 4 nhưng không chia hết cho 100 là năm nhuận
+                isLeap = true;
+        }
+        else {
+            isLeap = false;
+        }
+        if(isLeap==true)
+            System.out.println(year + " là năm nhuận.");
+        else
+            System.out.println(year + " không phải là năm nhuận.");
+
+        }
 
 
     }
 
 
-}
