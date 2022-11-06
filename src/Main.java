26fcbd56de275998d4cc7ecf7cc7f340812a79f7
unknown <trongtoanbk.hd@gmail.com>
2022-11-06 10:21:22 +0700

    Change Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        // 1. Kiểu dữ liệu
+        // Kiểu dữ liệu nguyên thủy (Primitive)
+        // EX: char, int, long, float, double, boolean
+        // Kiểu dữ liệu đối tượng (Object)
+        //EX: String, Interger, Long, Fload, Double,Boolean
+
+        // 2. Khai báo biến
+        char word = 'a';
+        int number1 =-1;
+        float number2 = 1.1f;
+        boolean isOnline = true;
+
+        String name = " Đỗ Trọng Toàn";
+        int tuoi = 31;
+        String soThich =" Thich hoc lap trinh";
+        double chieuCao = 1.78;
+        System.out.println(name);
+        System.out.println(tuoi);
+        System.out.println(soThich);
+        System.out.println(chieuCao);
+
     }
+
+
 }
