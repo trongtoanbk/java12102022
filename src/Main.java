5c88dbc9ec316259376a4b751c3d8b49ed6278c6
unknown <trongtoanbk.hd@gmail.com>
2022-11-06 10:58:24 +0700

    operator

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,49 @@ public class Main {
         //EX: String, Interger, Long, Fload, Double,Boolean
 
         // 2. Khai báo biến
-        char word = 'a';
-        int number1 =-1;
-        float number2 = 1.1f;
-        boolean isOnline = true;
+//        char word = 'a';
+//        int number1 =-1;
+//        float number2 = 1.1f;
+//        boolean isOnline = true;
+//
+//        String name = " Đỗ Trọng Toàn";
+//        int tuoi = 31;
+//        String soThich =" Thich hoc lap trinh";
+//        double chieuCao = 1.78;
+//        System.out.println(name);
+//        System.out.println(tuoi);
+//        System.out.println(soThich);
+//        System.out.println(chieuCao);
 
-        String name = " Đỗ Trọng Toàn";
-        int tuoi = 31;
-        String soThich =" Thich hoc lap trinh";
-        double chieuCao = 1.78;
-        System.out.println(name);
-        System.out.println(tuoi);
-        System.out.println(soThich);
-        System.out.println(chieuCao);
+        //3. Toán tử
+        //3.1 phep chia lay phan nguyen và lay phan du
+//        int a =10;
+//        int b=3;
+//        int result = a /b ; // lay gia tri nguyen
+//        int soDu = a %b ; // chia lay phan du
+//        System.out.println(result);
+//        System.out.println(soDu);
+
+        //3.2 toan tu ++/--
+        // Th1: a++
+        // 1: Gán giá trị vào cho a
+        //2: Tăng giá trị a lên 1 đơn vị
+        // TH2: ++a
+        //1: Tăng giá trị a lên 1 đơn vị
+        //2: gán giá trị vào cho a
+//        int a=10;
+//        int b= a++;
+//        int c= ++a;
+//        System.out.println(b);
+//        System.out.println(a);
+//        System.out.println(c);
+
+        int a= 5;
+        int b=5;
+        int result = a++ - --b + b-- +a-- - a++ + b++ - b--;
+        System.out.println(result);
+
+
 
     }
 
